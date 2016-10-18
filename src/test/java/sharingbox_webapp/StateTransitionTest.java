97dc42523d5abee50d4af41fb5ba8f2da8dc3918
MartianH <martial.habimana@gmail.com>
2016-10-18 22:05:40 +0200

    Fixed faulty test (again)

--- src/test/java/sharingbox_webapp/StateTransitionTest.java
+++ src/test/java/sharingbox_webapp/StateTransitionTest.java
@@ -22,15 +22,47 @@ public class StateTransitionTest {
         TestVerhuring verhuring = new TestVerhuring();
 
         //S1
-        verhuring.VerhuringAnnuleren();
+        verhuring.VerhuringToevoegen();
         //s2
-        verhuring.VerhuringOpheffen();
-        assertEquals("Begin punt (S2) -> (S1)", "In behandeling", verhuring.GetStatus());
+        verhuring.VerhuringAnnuleren();
+        assertEquals("Begin punt (S2) -> (S1)", "Geannuleerd", verhuring.GetStatus());
     }
 
     @Test
     public void S2_S1(){
         TestVerhuring verhuring = new TestVerhuring();
+
+        //S2
+        verhuring.VerhuringAnnuleren();
+        //s1
+        verhuring.VerhuringOpheffen();
+        assertEquals("Begin punt (S2) -> (S1)", "In behandeling", verhuring.GetStatus());
+    }
+
+    @Test
+    public void S1_S2_S2_S1(){
+        TestVerhuring verhuring = new TestVerhuring();
+
+        //S1
+        verhuring.VerhuringToevoegen();
+        //s2
+        verhuring.VerhuringAnnuleren();
+
+        //S2
+        verhuring.VerhuringAnnuleren();
+        //s1
+        verhuring.VerhuringOpheffen();
+        assertEquals("Begin punt (S2) -> (S1)", "In behandeling", verhuring.GetStatus());
+    }
+
+    @Test
+    public void S2_S1_S1_S2(){
+        TestVerhuring verhuring = new TestVerhuring();
+
+        //S2
+        verhuring.VerhuringAnnuleren();
+        //s1
+        verhuring.VerhuringOpheffen();
 
         //S1
         verhuring.VerhuringToevoegen();
