b72406f26539e145454451bb8f2811f2785df143
MartianH <martial.habimana@gmail.com>
2016-10-18 21:54:34 +0200

    Added Test verhuring class and Test Transition test

---
+++ src/test/java/sharingbox_webapp/StateTransitionTest.java
@@ -0,0 +1,41 @@
+package sharingbox_webapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by martialh on 10/18/16.
+ */
+public class StateTransitionTest {
+
+    @Test
+    public void S0_S1(){
+        TestVerhuring verhuring = new TestVerhuring();
+
+        assertEquals("Begin punt (S0)", "Aanvraag", verhuring.GetStatus());
+        verhuring.VerhuringToevoegen();
+        assertEquals("Begin punt (S0) -> (S1)", "In behandeling", verhuring.GetStatus());
+    }
+
+    @Test
+    public void S1_S2(){
+        TestVerhuring verhuring = new TestVerhuring();
+
+        //S1
+        verhuring.VerhuringAnnuleren();
+        //s2
+        verhuring.VerhuringOpheffen();
+        assertEquals("Begin punt (S2) -> (S1)", "Geannuleerd", verhuring.GetStatus());
+    }
+
+    @Test
+    public void S2_S1(){
+        TestVerhuring verhuring = new TestVerhuring();
+
+        //S1
+        verhuring.VerhuringToevoegen();
+        //s2
+        verhuring.VerhuringAnnuleren();
+        assertEquals("Begin punt (S2) -> (S1)", "In behandeling", verhuring.GetStatus());
+    }
+}
