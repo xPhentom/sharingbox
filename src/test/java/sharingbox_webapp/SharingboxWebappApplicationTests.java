ddcb9650c49e83e6e5a08dc378d9ec3c54a1ff6b
MartianH <martial.habimana@gmail.com>
2016-10-18 22:15:29 +0200

    necessary commit for merge

--- src/test/java/sharingbox_webapp/SharingboxWebappApplicationTests.java
+++ src/test/java/sharingbox_webapp/SharingboxWebappApplicationTests.java
@@ -1,11 +1,10 @@
-package sharingbox_CoverageTests;
+package sharingbox_webapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import sharingbox_webapp.sharingbox_webappApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = sharingbox_webappApplication.class)
